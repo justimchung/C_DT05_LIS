e27963f7d86c392efb26098501d51178a4cc0a6f
justimchung <justim@gmail.com>
2018-06-15 00:36:14 +0800

    加入 splitStr 函式，接受一個字串，依照某個字元切割字串。 完成 LIS 的第一版的功能。亦即可以回傳 LIS 長度

--- LIS/Source.cpp
+++ LIS/Source.cpp
@@ -1,8 +1,39 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<sstream>
+#include<string>
 using namespace std;
+int LIS(vector<int>& LISTbl);
+void splitStr(string s, vector<int>& buf, char c);
 int main()
 {
 
+}
+
+int LIS(vector<int>& LISTbl)
+{
+	if (LISTbl.size() == 0) return 0;
+	vector<int> LISLen(LISTbl.size(), 1);
+	for (int i = 1; i < LISTbl.size(); i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (LISTbl[j] < LISTbl[i])
+				LISLen[i] = max(LISLen[i], LISLen[j] + 1);
+		}
+	}
+
+	int maxlen = *max_element(LISLen.begin(), LISLen.end());
+	return maxlen;
+}
+
+void splitStr(string s, vector<int>& buf, char c)
+{
+	buf.clear();
+	stringstream ss(s);
+	string token;
+	while (getline(ss, token, c)) {
+		buf.push_back(stoi(token));
+	}
 }
