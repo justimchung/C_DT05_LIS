2243d08ca14feb78a77dec602a394c746a5b84e4
鍾毓驥 <justim@gmail.com>
2018-06-17 23:31:35 +0800

    更新 main 程式 更新 main 的內容，讓它可以正確呼叫 LIS

--- LIS/Source.cpp
+++ LIS/Source.cpp
@@ -17,7 +17,8 @@ int main()
 	cin >> stmp;
 	vector<int> buf;
 	splitStr(stmp, buf, ',');
-	cout << LIS(buf) << endl;
+	LIS(buf);
+	return 0;
 }
 
 int LIS(vector<int>& LISTbl)
@@ -34,7 +35,28 @@ int LIS(vector<int>& LISTbl)
 	}
 
 	int maxlen = *max_element(LISLen.begin(), LISLen.end());
-
+	int num, pos;
+	vector<int> buf;
+	getMaxElementAndPos(LISTbl, LISLen,
+		numeric_limits<int>::max(),
+		maxlen, LISTbl.size() - 1, num, pos);
+	buf.push_back(num);
+	for (int len = maxlen - 1; len >= 1; len--)
+	{	
+		int tnum = num;
+		int tpos = pos;
+		getMaxElementAndPos(LISTbl, LISLen,
+			tnum, len, tpos - 1, num, pos);
+		buf.push_back(num);
+	}
+	reverse(buf.begin(), buf.end());
+	for (int k = 0; k < buf.size(); k++)
+	{
+		if (k == buf.size() - 1)
+			cout << buf[k] << endl;
+		else
+			cout << buf[k] << ",";
+	}
 	return maxlen;
 }
 
